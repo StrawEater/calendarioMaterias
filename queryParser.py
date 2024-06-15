0118736d97303e33268e4b0344244784ed0b67c0
juanB <juani8.bustos@gmail.com>
2024-06-15 14:58:20 -0300

    Se guarda el diccionario en un json y se comento mas codigo, se refactorizo la parte de subcomisiones para normalizar la manera

--- queryParser.py
+++ queryParser.py
@@ -1,5 +1,6 @@
 import re
 from bs4 import BeautifulSoup
+import json
 
 # Abrimos el archivo donde esta el Source Code de la pagina
 pageFile = open("page.html", "r")
@@ -13,98 +14,151 @@ pageSource = pageFile.read()
 #Tenemos que formatear usando literales \\, ya que toda las informacion estar escrita en un script de una linea que escribe su contenido directamente a la pagina.
 regexQueryDivPeriodo = r"(<div class=\\\"js-recuadro_periodo recuadro\\\"(.*?)>)(.*?)(?=(<div id=\\\"js-reporte-vacio\\\")(.*?)<\\/div>)"
 
-#Conseguimos
+#Conseguimos un iterador con cada match en el codigo fuente(cada <div> de periodo )
 matches = re.finditer(regexQueryDivPeriodo, pageSource)
 
+#Inicializamos un diccionario donde guardar la informacion de cada Periodo Lectivo indexeado por su id
 periodosLectivosDic = {}
 
+#Populamos el diccionario de Periodos Lectivos
 for match in matches:
     
+    # Formateamos el div encontrado remplazando los literales por su valor real
+    # \\t -> \t | \\n -> \n | \\/ -> /
+    # Tenemos el problema de los caracteres de escape como "\\u00ed" -> "\u00ed" -> í
+    # TODO: hacer una funcion que remplaze los valores de los caracteres de escape (facil)
     formatedPageSourceCode = match.group().replace("\\t","\t").replace("\\n","\t").replace("\\\"","\"").replace("\\/","/")
+    
+    #Parseamos el codigo por BeautifulSoup 
     soup = BeautifulSoup(formatedPageSourceCode, "html.parser")
-        
+    
     periodoDic = {}
+    # Inicializamos un diccionario que representara al periodo:
+    # [id] : id del periodo lectivo
+    # [nombre] : nombre del periodo lectivo
+    # [nombre] : nombre del periodo lectivo
+    # [actividades] : diccionario con la cantidad de actividades del periodo y la informacion de cada una
+    
+    # Parseamos el div principal, donde se encuentra toda la informacion del periodo
     periodo = soup.find("div", class_="js-recuadro_periodo recuadro")
-        
+    
+    # Guardamos el id del periodo, el cual esta como atributo personalizado
     periodoDic["id"] = periodo["periodo"]
-        
+    
+    # Buscamos el nombre del periodo
     periodoNombre = periodo.find("h3")
     periodoNombreText = periodoNombre.text
+    #El texto esta formateado como: "Período lectivo: NOMBRE"
+    #Manipulamos el string para solo quedarnos con "NOMBRE"
     periodoNombreText = periodoNombreText[periodoNombreText.rfind(":") + 1 : ].strip()
+    # Guardamos el nombre del periodo
     periodoDic["nombre"] = periodoNombreText
-        
+    
+    #Inicializamos el diccionario para guardar la informacion de las actividades que suceden en el periodo lectivo
+    # [cantidad] : Cantidad de actividades que forman parte de este periodo lectivo
+    # [actividades] : Diccionario con la informacion de cada actividad indexeado por el id de la actividad
     actividadesDic = {}
+    
+    # Cada actividad es contenida en un <div> con la clase "js-recuadro_actividad"
+    # Encontramos cada uno de estos <div> dentro del periodo, consiguiendo una lista de cada actividad
     actividades = periodo.find_all("div",class_="js-recuadro_actividad")
-        
+    
+    # Guardamos la cantidad de actividades e inicializamos el diccionario de actividades
     actividadesDic["cantidad"] = len(actividades)
     actividadesDic["actividades"] = {}
-        
+    
+    # Recorremos cada <div> actividad y populamos el diccionario de actividades
     for actividad in actividades:
-            
+        
+        #Inicializamos un diccionario para representar cada actividad
+        # [id] : id de la actividad
+        # [nombre] : nombre de la actividad
+        # [comisiones] : Diccionario con la informacion de cada comision de la actividad, indexeado por su nombre
         actividadDic = {}
-            
+        
+        #Conseguimos Nombre de la actividad por un atributo personalizado del <div> de actividad
+        #el atributo esta formateado como: "NOMBRE (ID)"
         nombreActividad = actividad["actividad"]
+        
+        #Manipulamos el string para aislar ID
         idActividad = nombreActividad[nombreActividad.rfind("(") + 1 : -1]
-            
+        
+        #Guardamos el id y nombre de la actividad
         actividadDic["id"] = idActividad 
         actividadDic["nombre"] = nombreActividad
+        
+        #Inicializamos el diccionario que representar cada comision de la actividad
         actividadDic["comisiones"] = {}
 
+        # Cada comision esta contenida en su propia <table> con la clases "table table-bordered table-condensed comision"
+        # Encontramos cada tabla de comision y las parseamos
         tablasInformacionComisiones = actividad.find_all("table", class_="table table-bordered table-condensed comision")
         for tablaComision in tablasInformacionComisiones:
-            
-            filasComisiones = tablaComision.find_all("tr", class_="comision")
-                
-            filaInformacionComision = filasComisiones[0] 
-                
-            columnasInformacionComision = list(filaInformacionComision.children)
-             
-            columnaNombreComision = columnasInformacionComision[0]
-            columnaTurnoComision = columnasInformacionComision[1]
-            columnaInfoComision = columnasInformacionComision[2]
-
+            #Inicializamos el diccionario que representara a las comisiones
+            # [nombre] : nombre de la comision
+            # [turno] : turno de la comision
+            # [instancia] : tipo de comision
+            # [ubicacion] : ubicacion de la comision
+            # [observacion] : observacion OPCIONAL de la comision
+            # [subcomisiones] : Diccionario con la informacion de cada subcomision, indexeado por su nombre
             comisionDir = {}
-
-            nombreComision = columnaNombreComision.find("h5").text
-            nombreComision = nombreComision[nombreComision.find("n:") + 2 : ].strip()
-            comisionDir["nombre"] = nombreComision
-                
-            turnoComision = columnaTurnoComision.find("span").text
-            turnoComision = turnoComision[turnoComision.find(":") + 2 : ].strip()
-            comisionDir["turno"] = turnoComision
-
-            comisionInstancia, comisionUbicacion = columnaInfoComision.find_all("span")
-                
-            comisionInstanciaText = comisionInstancia.text
-            comisionInstanciaText = comisionInstanciaText[comisionInstanciaText.find(":") + 2 : ].strip()
-            comisionDir["instancia"] = comisionInstanciaText
-
-            comisionUbicacionText = comisionUbicacion.text
-            comisionUbicacionText = comisionUbicacionText[comisionUbicacionText.find(":") + 2 : ].strip()
-            comisionDir["ubicacion"] = comisionUbicacionText
-
-            if(len(filasComisiones) > 1):
-                filaObservacionComision = filasComisiones[1]
-                ObservacionComision = filaObservacionComision.find("td")
-                ObservacionComisionText = ObservacionComision.text
-                ObservacionComisionText = ObservacionComisionText[ObservacionComisionText.find(":") + 2 : ].strip()
-                comisionDir["observacion"] = ObservacionComisionText
-            else:
-                comisionDir["observacion"] = ""
-
+            
+            #Inicializamos el diccionario que representara cada subcomision de la comision
             comisionDir["subcomisiones"] = {}
+            
+            # La informacion de las subcomisiones no estan contenidas en si misma como lo estaban el resto de objetos
+            # Cada Tabla de informacion esta formateada de la siguiente forma:
+            # <tr>
+            
             ultimaSubComision = ""
-            for filasSubComisionesInfo in tablaComision.find_all("tr"):
+            comisionEncontrada = False
+            
+            for filasComisionesInfo in tablaComision.find_all("tr"):
                     
-                    if filasSubComisionesInfo["class"] == ["comision"]:
+                    if filasComisionesInfo["class"] == ["comision"]:
+                        
+                        if(not(comisionEncontrada)):
+                            columnasInformacionComision = list(filasComisionesInfo.children)
+                            
+                            columnaNombreComision = columnasInformacionComision[0]
+                            columnaTurnoComision = columnasInformacionComision[1]
+                            columnaInfoComision = columnasInformacionComision[2]                            
+
+                            nombreComision = columnaNombreComision.find("h5").text
+                            nombreComision = nombreComision[nombreComision.find("n:") + 2 : ].strip()
+                            comisionDir["nombre"] = nombreComision
+                    
+                            turnoComision = columnaTurnoComision.find("span").text
+                            turnoComision = turnoComision[turnoComision.find(":") + 2 : ].strip()
+                            comisionDir["turno"] = turnoComision
+
+                            comisionInstancia, comisionUbicacion = columnaInfoComision.find_all("span")
+                                
+                            comisionInstanciaText = comisionInstancia.text
+                            comisionInstanciaText = comisionInstanciaText[comisionInstanciaText.find(":") + 2 : ].strip()
+                            comisionDir["instancia"] = comisionInstanciaText
+
+                            comisionUbicacionText = comisionUbicacion.text
+                            comisionUbicacionText = comisionUbicacionText[comisionUbicacionText.find(":") + 2 : ].strip()
+                            comisionDir["ubicacion"] = comisionUbicacionText
+                            
+                            comisionDir["observacion"] = ""
+                            
+                            comisionEncontrada = True
+                            
+                        else:
+                            
+                            ObservacionComision = filasComisionesInfo.find("td")
+                            ObservacionComisionText = ObservacionComision.text
+                            ObservacionComisionText = ObservacionComisionText[ObservacionComisionText.find(":") + 2 : ].strip()
+                            comisionDir["observacion"] = ObservacionComisionText
+                                    
+                    if filasComisionesInfo["class"] == ["horarios", "encabezados"]:
                         continue
 
-                    if filasSubComisionesInfo["class"] == ["horarios", "encabezados"]:
-                        continue
-
-                    if filasSubComisionesInfo["class"] == ["subcomision"]:
+                    if filasComisionesInfo["class"] == ["subcomision"]:
                        
-                        columnasInformacionSubComisiones = list(filasSubComisionesInfo.children)
+                        columnasInformacionSubComisiones = list(filasComisionesInfo.children)
              
                         columnaNombreSubComision = columnasInformacionSubComisiones[0]
                         columnaCupoSubComision = columnasInformacionSubComisiones[1]
@@ -142,9 +196,9 @@ for match in matches:
 
                         comisionDir["subcomisiones"][subComisionDir["nombre"]] = subComisionDir
 
-                    if "js-dia" in filasSubComisionesInfo["class"]:
+                    if "js-dia" in filasComisionesInfo["class"]:
                         
-                        columnasInformacionDias = list(filasSubComisionesInfo.children)
+                        columnasInformacionDias = list(filasComisionesInfo.children)
              
                         columnaTipoClase = columnasInformacionDias[0]
                         columnaDia = columnasInformacionDias[1]
@@ -170,16 +224,20 @@ for match in matches:
             
         actividadesDic["actividades"][actividadDic["id"]] = actividadDic
         
-    periodoDic["actividades"] = actividadesDic
+    periodoDic["actividadesInformacion"] = actividadesDic
 
     periodosLectivosDic[periodoDic["id"]] = periodoDic
     
-    print()
-    print(periodoDic["id"])
-    print(periodoDic)
-    print()
-    
+    # print()
+    # print(periodoDic["id"])
+    # print(periodoDic)
+    # print()
 
+ejemploDiccionario = open("ejemploDiccionario.json", "w")
+
+json.dump(periodosLectivosDic, ejemploDiccionario)
+
+ejemploDiccionario.close()
 
 
 
